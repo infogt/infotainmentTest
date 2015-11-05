bcf59aa76f03abc120275225093eee21b8c92b53
Senthil <senthil@sentil.com>
2015-11-05 15:02:13 +0100

    1st commit

---
+++ Unit_Test/include/factorial.h
@@ -0,0 +1,13 @@
+/*
+ * factorial.h
+ *
+ *  Created on: 04.11.2015
+ *      Author: sthangavelmurugesa
+ */
+
+#ifndef FACTORIAL_H_
+#define FACTORIAL_H_
+
+int factorial(int n);
+
+#endif /* FACTORIAL_H_ */
